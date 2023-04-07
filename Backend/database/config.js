7fc1db1b602b63070d61eddf5b8bf1885cc700c4
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-04-06 20:08:23 -0400

    Scraper products routes

---
+++ Backend/database/config.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const mongoose = require('mongoose')
+
+const dbConnection = async() => {
+  try {
+    await mongoose.connect(process.env.DB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log('DB corriendo correctamente')
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error a la hora de inicializar DB')
+  }
+}
+
+module.exports = { dbConnection }
