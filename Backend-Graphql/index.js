1312283eae9270e4b1c57b81d660d6aa72c903a9
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-06-08 22:59:46 -0400

    add getBestDiscountProducts

--- Backend-Graphql/index.js
+++ Backend-Graphql/index.js
@@ -1,10 +1,10 @@
 import { ApolloServer, gql } from 'apollo-server'
 import './database/config.js'
 // import Product from './models/Product.js'
-import { getProducts, totalPages, totalProducts } from './resolvers/product.js'
+import { getBestDiscountProducts, getProducts, totalPages, totalProducts } from './resolvers/product.js'
 
 const typeDefinitions = gql`
-  type UnitProduct {
+  type ProductAPI {
     _id: ID!
     name: String!
     brand: String!
@@ -29,12 +29,31 @@ const typeDefinitions = gql`
     last_hash: String!
   }
 
-  type Product {
+  type ProductDB {
     _id: ID!
     title: String!
-    product: UnitProduct!
+    product: ProductAPI!
     quantity: Int!
     websites: [Website]
+    image_path: String!
+  }
+
+  type Product {
+    id: ID!
+    title: String!
+    brand: String!
+    alcoholic_grade: Float!
+    content: Int!
+    best_price: Int!
+    image_path: String!
+  }
+
+  type BestDiscountProduct {
+    id: ID!
+    title: String!
+    brand: String!
+    discount: Int!
+    best_price: Int!
     image_path: String!
   }
 
@@ -42,10 +61,19 @@ const typeDefinitions = gql`
     category: String!
   }
 
+  enum OrderByEnum {
+    SCORE_DESC,
+    PRICE_DESC,
+    PRICE_ASC,
+    NAME_ASC,
+    NAME_DESC
+  }
+
   type Query {
     totalProducts(filters: FiltersInput!): Int!
     totalPages(filters: FiltersInput!): Int!
-    getProducts(orderBy: String!, page: Int!, filters: FiltersInput!): [Product]!
+    allProducts(orderBy: OrderByEnum!, page: Int!, filters: FiltersInput!): [Product]!
+    bestDiscountProducts: [BestDiscountProduct]!
   }
 `
 
@@ -53,7 +81,29 @@ const resolvers = {
   Query: {
     totalProducts,
     totalPages,
-    getProducts
+    allProducts: getProducts,
+    bestDiscountProducts: getBestDiscountProducts
+  },
+  Product: {
+    id: (root) => {
+      const idString = root._id.toString()
+      const pidString = root.product._id.toString()
+      return idString.substring(idString.length - 3) + pidString.substring(pidString.length - 3)
+    },
+    brand: (root) => root.product.brand,
+    alcoholic_grade: (root) => root.product.alcoholic_grade,
+    content: (root) => root.product.content,
+    best_price: (root) => root.websites[0].best_price
+  },
+  BestDiscountProduct: {
+    id: (root) => {
+      const idString = root._id.toString()
+      const pidString = root.product._id.toString()
+      return idString.substring(idString.length - 3) + pidString.substring(pidString.length - 3)
+    },
+    brand: (root) => root.product.brand,
+    discount: (root) => Math.round(100 - ((root.websites[0].best_price * 100) / root.websites[0].price)),
+    best_price: (root) => root.websites[0].best_price
   }
 }
 
--- Backend-Graphql/resolvers/product.js
+++ Backend-Graphql/resolvers/product.js
@@ -15,11 +15,44 @@ const totalPages = async (root, args) => {
 }
 
 const getProducts = async (root, args) => {
+  const { orderBy, page, filters } = args
+  console.log(orderBy)
+  console.log(page)
+  console.log(filters)
+
   return Product.find({})
+}
+
+const getBestDiscountProducts = async () => {
+  const products = await Product.aggregate([
+    { $unwind: '$websites' },
+    { $sort: { 'websites.best_price': 1 } },
+    {
+      $group: {
+        _id: '$_id',
+        websites: { $push: '$websites' },
+        otherFields: { $first: '$$ROOT' }
+      }
+    },
+    { $replaceRoot: { newRoot: { $mergeObjects: ['$otherFields', { websites: '$websites' }] } } }
+  ])
+
+  // ordena los productos mediante el mayor descuento > menor precio > titulo alfabetico
+  products.sort((a, b) => {
+    const discountA = Math.round(100 - (a.websites[0].best_price * 100) / a.websites[0].price)
+    const discountB = Math.round(100 - (a.websites[0].best_price * 100) / a.websites[0].price)
+
+    if (discountA !== discountB) return discountB - discountA
+    if (a.websites[0].best_price !== b.websites[0].best_price) return a.websites[0].best_price - b.websites[0].best_price
+    return a.title.localCompare(b.title)
+  })
+
+  return products.slice(0, 7)
 }
 
 export {
   totalProducts,
   totalPages,
-  getProducts
+  getProducts,
+  getBestDiscountProducts
 }
