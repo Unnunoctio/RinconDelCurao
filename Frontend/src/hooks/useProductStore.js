76cf3bf13597fa03cfeef7b5f9c77b994581c202
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-06-13 03:08:01 -0400

    useProductStore with useLazyQuery

--- Frontend/src/hooks/useProductStore.js
+++ Frontend/src/hooks/useProductStore.js
@@ -1,13 +1,14 @@
+import { useEffect } from 'react'
+import { gql, useLazyQuery } from '@apollo/client'
 import { shallow } from 'zustand/shallow'
-import { ProductStore, fetchImage } from '../store'
-import productsApi from '../api/productsApi'
+import { ProductStore } from '../store'
 
-const queryProduct = `
-  query ($productId: ID!, $title: String!) {
+const GET_PRODUCT = gql`
+  query GetProduct($productId: ID!, $title: String!) {
     product(id: $productId, title: $title) {
       title
       quantity
-      image_path
+      image
       product {
         brand
         alcoholic_grade
@@ -32,8 +33,25 @@ const queryProduct = `
 `
 
 export const useProductStore = () => {
+  const [getProductData, { error, data }] = useLazyQuery(GET_PRODUCT)
+
   const [isLoading, product, isError] = ProductStore((state) => [state.isLoading, state.product, state.isError], shallow)
   const [handleLoading, handleProduct, handleError, resetStore] = ProductStore((state) => [state.handleLoading, state.handleProduct, state.handleError, state.resetStore], shallow)
+
+  useEffect(() => {
+    if (error) {
+      console.error(error.message)
+      handleError(true)
+      handleLoading(false)
+    }
+    if (data) {
+      console.log(data)
+      setTimeout(() => {
+        handleProduct(data.product)
+        handleLoading(false)
+      }, 500)
+    }
+  }, [data, error])
 
   const getProduct = async (url) => {
     handleLoading(true)
@@ -42,17 +60,11 @@ export const useProductStore = () => {
     try {
       const id = url.substring(0, 6)
       const title = url.substring(7)
-
-      const { product } = await productsApi(queryProduct, { productId: id, title })
-      product.image = await fetchImage(product.image_path)
-
-      handleProduct(product)
+      getProductData({ variables: { productId: id, title } })
     } catch (error) {
-      console.log(error)
       handleError(true)
+      handleLoading(false)
     }
-
-    handleLoading(false)
   }
 
   const resetProduct = () => {
