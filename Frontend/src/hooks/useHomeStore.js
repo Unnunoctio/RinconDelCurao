2057c06a214bb78ddce1e9e2f2219dbbb7ea2171
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-06-13 02:17:19 -0400

    useHomeStore with useLazyQuery

--- Frontend/src/hooks/useHomeStore.js
+++ Frontend/src/hooks/useHomeStore.js
@@ -1,6 +1,8 @@
 import { shallow } from 'zustand/shallow'
 import { HomeStore, fetchImage } from '../store'
 import productsApi from '../api/productsApi'
+import { gql, useLazyQuery } from '@apollo/client'
+import { useEffect } from 'react'
 
 const queryProducts = `
   query {
@@ -15,26 +17,41 @@ const queryProducts = `
   }
 `
 
+const GET_BEST_DISCOUNT_PRODUCTS = gql`
+  query GetBestDiscountProducts {
+    bestDiscountProducts {
+      id
+      title
+      brand
+      discount
+      best_price
+      image_path
+    }
+  }
+`
+
 export const useHomeStore = () => {
+  const [getProducts, { error, data }] = useLazyQuery(GET_BEST_DISCOUNT_PRODUCTS)
+
   const [isLoading, offerProducts] = HomeStore((state) => [state.isLoading, state.offerProducts], shallow)
   const [handleLoading, handleOfferProducts] = HomeStore((state) => [state.handleLoading, state.handleOfferProducts], shallow)
 
+  useEffect(() => {
+    if (error) {
+      console.error(error.message)
+      handleLoading(false)
+    }
+    if (data) {
+      setTimeout(() => {
+        handleOfferProducts(data.bestDiscountProducts)
+        handleLoading(false)
+      }, 500)
+    }
+  }, [data, error])
+
   const getHomeProducts = async () => {
     handleLoading(true)
-
-    try {
-      const { bestDiscountProducts } = await productsApi(queryProducts)
-      await Promise.all(bestDiscountProducts?.map(async product => {
-        product.image = await fetchImage(product.image_path)
-      }))
-
-      handleOfferProducts(bestDiscountProducts)
-    } catch (error) {
-      console.log(error)
-      handleOfferProducts([])
-    }
-
-    handleLoading(false)
+    getProducts()
   }
 
   return {
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from '@apollo/client'
+import { apolloClient } from './api/apolloClient'
 
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
