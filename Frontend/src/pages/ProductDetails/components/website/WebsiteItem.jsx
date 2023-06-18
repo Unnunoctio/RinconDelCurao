6999ed8be5993fd53b8ac8017eea039ed7298387
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-06-18 03:39:15 -0400

    add component websiteSvg

--- Frontend/src/pages/ProductDetails/components/website/WebsiteItem.jsx
+++ Frontend/src/pages/ProductDetails/components/website/WebsiteItem.jsx
@@ -1,4 +1,5 @@
-import { Flex, HStack, Heading, Icon, Text, useColorModeValue } from '@chakra-ui/react'
+import { Flex, HStack, Heading, Text, useColorModeValue } from '@chakra-ui/react'
+import { WebsiteSvg } from './WebsiteSvg'
 
 export const WebsiteItem = ({ website }) => {
   return (
@@ -6,7 +7,7 @@ export const WebsiteItem = ({ website }) => {
       <HStack
         minW='264px' w='full'
         gap={{ base: 0, sm: 5 }}
-        py={2} px={4}
+        py={2} px={{ base: 2, sm: 4 }}
         background={useColorModeValue('light.background.main', 'dark.background.main')}
         justifyContent='center'
         boxShadow='md'
@@ -18,17 +19,17 @@ export const WebsiteItem = ({ website }) => {
           borderColor: useColorModeValue('light.divider.active', 'dark.divider.active')
         }}
       >
-        <Flex minW={12} justifyContent='center' display={{ base: 'none', sm: 'flex' }}>
-          <Icon boxSize={{ base: 12, sm: 14 }} />
+        <Flex minW={12} justifyContent='center'>
+          <WebsiteSvg websiteName={website.name} boxSize={{ base: 12, sm: 14 }} />
         </Flex>
-        <Flex flexDir='column' gap={2} w={{ base: 'full', sm: 'auto' }}>
+        <Flex flexDir='column' gap={{ base: 0, sm: 2 }} w={{ base: 'full', sm: 'auto' }}>
           <Heading
             fontSize={20} fontWeight='medium'
             color={useColorModeValue('light.text.main', 'dark.text.main')}
           >
             {website.name}
           </Heading>
-          <HStack gap={4}>
+          <HStack gap={{ base: 0, sm: 4 }} justifyContent='space-between'>
             <PriceItem title='Precio Oferta' value={website.best_price} color={useColorModeValue('light.text.active', 'dark.text.active')} />
             <PriceItem title='Precio Normal' value={website.price} color={useColorModeValue('light.text.main', 'dark.text.main')} />
           </HStack>
@@ -42,7 +43,7 @@ const PriceItem = ({ title, value, ...rest }) => {
   return (
     <Flex flexDir='column' alignItems='flex-end'>
       <Text
-        fontSize={16}
+        fontSize={{ base: 14, sm: 16 }}
         color={useColorModeValue('light.text.secondary', 'dark.text.secondary')}
       >
         {title}
