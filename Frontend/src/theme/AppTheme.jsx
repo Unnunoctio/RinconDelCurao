b53ab824a7eee4fbea245a094671b671bd5a0be9
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2023-07-02 14:07:25 -0400

    style: add custom scrollbar

--- Frontend/src/theme/AppTheme.jsx
+++ Frontend/src/theme/AppTheme.jsx
@@ -1,14 +1,41 @@
-import { ChakraProvider, ColorModeScript, CSSReset } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, CSSReset, useColorMode } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 import { theme } from './theme'
 
 export const AppTheme = ({ children }) => {
   return (
     <ChakraProvider theme={theme}>
-      <CSSReset />
-      <Global />
+      {/* <CSSReset />
+      <Global /> */}
+      <CustomScrollbar />
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       {children}
     </ChakraProvider>
+  )
+}
+
+const CustomScrollbar = () => {
+  const { colorMode } = useColorMode()
+
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={{
+          '::-webkit-scrollbar': {
+            width: '8px',
+            borderRadius: '8px',
+            backgroundColor: colorMode === 'light' ? '#FFFFFF' : '#171923'
+          },
+          '::-webkit-scrollbar-thumb': {
+            borderRadius: '8px',
+            backgroundColor: colorMode === 'light' ? '#D69E2E99' : '#805AD599'
+          },
+          '::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: colorMode === 'light' ? '#D69E2E' : '#805AD5'
+          }
+        }}
+      />
+    </>
   )
 }
